826f22415f65d2ee77948664f6b0af2bac285af3
Alexander Tretyakov <tpetyakov@gmail.com>
2022-06-13 14:09:37 +0300

    feat: complete 05-objects-tasks

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,8 +20,14 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    getArea: function() {
+      return this.width * this.height;
+    }
+  }
 }
 
 
@@ -35,8 +41,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +57,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = new proto.constructor();
+  Object.assign(obj, JSON.parse(json));
+  return obj;
 }
 
 
@@ -111,35 +119,111 @@ function fromJSON(/* proto, json */) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  add(value, type) {
+    if (!(this instanceof MyCssSelector)) {
+      return (new MyCssSelector()).add(value, type);
+    }
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  element(value) {
+    return this.add(value, MyCssSelector.ITEM_TYPES.ELEMENT);
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    return this.add(value, MyCssSelector.ITEM_TYPES.ID);
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    return this.add(value, MyCssSelector.ITEM_TYPES.CLASS);
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    return this.add(value, MyCssSelector.ITEM_TYPES.ATTR);
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    return this.add(value, MyCssSelector.ITEM_TYPES.PSEUDO_CLASS);
   },
 
-  combine(/* selector1, combinator, selector2 */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    return this.add(value, MyCssSelector.ITEM_TYPES.PSEUDO_ELEMENT);
   },
+
+  combine(selector1, combinator, selector2) {
+    return selector1
+      .add(combinator, MyCssSelector.ITEM_TYPES.COMBINATOR)
+      .add( selector2, MyCssSelector.ITEM_TYPES.SELECTOR);
+  }
 };
 
+function ICssBuilder() {}
+Object.keys(cssSelectorBuilder).forEach((key) => {
+  ICssBuilder.prototype[key] = cssSelectorBuilder[key];
+});
+ICssBuilder.prototype.constructor = ICssBuilder;
+
+class MyCssSelector extends ICssBuilder {
+  constructor(items = []) {
+    super();
+    this.items = items;
+    const T = MyCssSelector.ITEM_TYPES;
+    this.disposableTypes = [T.ELEMENT, T.ID, T.PSEUDO_ELEMENT];
+  }
+
+  validate(type) {
+    const T = MyCssSelector.ITEM_TYPES;
+    const typesCounter = this.disposableTypes.reduce((acc, t) => {
+      if (!acc[t]) acc[t] = 0;
+      acc[t] = this.items.filter(({ type }) => type === t).length;
+      return acc;
+    }, {});
+    if (this.disposableTypes.includes(type) && typesCounter[type] === 1) {
+      throw new Error('Element, id and pseudo-element should not occur more then one time inside the selector');
+    }
+    const lastItem = this.items[this.items.length - 1];
+    if (lastItem && type < lastItem.type) {
+      throw new Error('Selector parts should be arranged in the following order: element, id, class, attribute, pseudo-class, pseudo-element');
+    }
+  }
+
+  add(value, type) {
+    const T = MyCssSelector.ITEM_TYPES;
+    if (type !== T.SELECTOR) {
+      this.validate(type);
+    }
+    let newValue = '';
+    switch(type) {
+      case T.ID: newValue = `#${value}`; break;
+      case T.CLASS: newValue = `.${value}`; break;
+      case T.ATTR: newValue = `[${value}]`; break;
+      case T.PSEUDO_CLASS: newValue = `:${value}`; break;
+      case T.PSEUDO_ELEMENT: newValue = `::${value}`; break;
+      case T.COMBINATOR: newValue = ` ${value} `; break;
+      case T.ELEMENT: default: newValue = value; break;
+    }
+    if (type === T.SELECTOR) {
+      this.items.push(...newValue.items);
+    } else {
+      this.items.push({ value: newValue, type });
+    }
+    return this;
+  }
+
+  stringify() {
+    return this.items.map(({ value }) => value).join('');
+  }
+}
+
+MyCssSelector.ITEM_TYPES = {
+  ELEMENT: 0,
+  ID: 1,
+  CLASS: 2,
+  ATTR: 3,
+  PSEUDO_CLASS: 4,
+  PSEUDO_ELEMENT: 5,
+  COMBINATOR: 6,
+  SELECTOR: 7,
+}
 
 module.exports = {
   Rectangle,
